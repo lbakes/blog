a180e0051d534e884b03738098305e4eaaa506ca
Lauren Baker <lbaker@techtonicgroup.com>
2017-07-15 12:46:05 -0600

    Heroku config

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'actionpack-action_caching'
+gem 'rails_12factor', group: :production
 
 gem 'pg', '~> 0.20'
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,4 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/ unless Rails.env.test?
 end
